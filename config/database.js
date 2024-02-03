710fc4b9bf3a49bfa7a540d16b9622200ef0056f
quppiehy <sunhuiyi@gmail.com>
2024-02-03 22:00:09 +0800

    new repo basic

---
+++ config/database.js
@@ -0,0 +1,64 @@
+require("dotenv").config();
+
+module.exports = {
+  testing: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      useUTC: false,
+      dateStrings: true,
+      typeCast: function (field, next) {
+        // for reading from database
+        if (field.type === "DATETIME") {
+          return field.string();
+        }
+        return next();
+      },
+    },
+    timezone: "+08:00",
+  },
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      useUTC: false,
+      dateStrings: true,
+      typeCast: function (field, next) {
+        // for reading from database
+        if (field.type === "DATETIME") {
+          return field.string();
+        }
+        return next();
+      },
+    },
+    timezone: "+08:00",
+  },
+  production: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+    dialectOptions: {
+      useUTC: false,
+      dateStrings: true,
+      typeCast: function (field, next) {
+        // for reading from database
+        if (field.type === "DATETIME") {
+          return field.string();
+        }
+        return next();
+      },
+    },
+    timezone: "+08:00",
+  },
+};
